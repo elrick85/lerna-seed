b08a6af3e1ea3b45d465cf9098eadeebb4c593ed
zismailov <zaur_ismailov@epam.com>
2019-02-05 12:25:41 +0300

    Update message

--- src/service/index.js
+++
@@ -1,6 +0,0 @@
-const {coreFn} = require('@myscope/core');
-const {loggerFn} = require('@myscope/logger');
-
-coreFn();
-loggerFn();
-console.log('My service');
---
+++ src/service/src/index.ts
@@ -0,0 +1,7 @@
+import {coreFn} from '@myscope/core';
+import {loggerFn} from '@myscope/logger';
+
+coreFn();
+loggerFn();
+
+console.log('My service');
